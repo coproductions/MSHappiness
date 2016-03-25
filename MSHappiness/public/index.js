f9887587f11c451de6a94bba8d8b79ec54cc7617
Constantin Dumba <cocodini@hotmail.com>
2016-03-25 15:24:25 -0700

    adds ajax example

--- MSHappiness/public/index.js
+++ MSHappiness/public/index.js
@@ -1,1 +1,13 @@
 ﻿console.log('hello from index.js');
+$(".helloButton").click(function () {
+    console.log('clicked');
+    
+    
+    $.ajax({
+        url: "http://localhost:1337/test1",
+
+    }).done(function (data){
+        console.log('data', data);
+        $('.hello').html(data[0].building);
+    })
+});
--- MSHappiness/server.js
+++ MSHappiness/server.js
@@ -4,10 +4,16 @@ var port = process.env.port || 1337;
 var path = require('path');
 var app = express();
 var dummyData = require('./dummyData.js');
+
+//designating public folder contents to be served up to the client
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/test1', function (req, res) {
+    res.send(dummyData.people);
 });
 app.listen(port, function () {
     console.log('listening on port: ' + port);
