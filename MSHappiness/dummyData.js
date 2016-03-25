b5b98f4e0a0cdc169b69ebc14ba703598bc44279
Constantin Dumba <cocodini@hotmail.com>
2016-03-25 14:39:19 -0700

    added dummy data file

---
+++ MSHappiness/dummyData.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+    "people": [
+        {
+            "building": "25",
+            "id": "p1",
+            "surveys": ["s1", "s3"]
+        }
+    ],
+    "surveys": [
+        {
+            "comment": "blah blah blah",
+            "id": "s1",
+            "question": 1,
+            "response": 1
+        }
+    ]
+}
--- MSHappiness/server.js
+++ MSHappiness/server.js
@@ -3,12 +3,11 @@ var fs = require('fs');
 var port = process.env.port || 1337;
 var path = require('path');
 var app = express();
-var publicPath = "c:/users\v-codumb\documents\visual studio 2015\Projects/Ms";
+var dum = fs.readFileSync(__dirname + "/dummyData.json", 'utf8');
+var dummyData = require('./dummyData.js');
+
 app.get('/', function (req, res) {
-  //  res.sendFile('/index.html');
     res.sendFile(__dirname+'/public/index.html');
-   // res.send('hello');
- // res.sendFile('c:\Users\v-codumb\Documents\Visual Studio 2015\Projects\MSHappiness\MSHappiness\public\index.html');
 });
 app.listen(port, function () {
     console.log('listening on port: ' + port);
