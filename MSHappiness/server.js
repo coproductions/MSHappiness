09bac23c2753b9b3e0d00464f2e1947793406de8
Constantin Dumba <cocodini@hotmail.com>
2016-03-24 17:46:31 -0700

    serves index page

--- MSHappiness/server.js
+++ MSHappiness/server.js
@@ -1,15 +1,15 @@
-﻿var http = require('http');
-var express = require('express');
+﻿var express = require('express');
 var fs = require('fs');
 var port = process.env.port || 1337;
-
+var path = require('path');
 var app = express();
-
-app.get('/', function (req, res){
-    res.send("hello");
-})
-
+var publicPath = "c:/users\v-codumb\documents\visual studio 2015\Projects/Ms";
+app.get('/', function (req, res) {
+  //  res.sendFile('/index.html');
+    res.sendFile(__dirname+'/public/index.html');
+   // res.send('hello');
+ // res.sendFile('c:\Users\v-codumb\Documents\Visual Studio 2015\Projects\MSHappiness\MSHappiness\public\index.html');
+});
 app.listen(port, function () {
     console.log('listening on port: ' + port);
 });
-//test2
