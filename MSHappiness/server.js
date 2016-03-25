9a93223ea7efd5eddbb7bcf62c04120966b20415
Constantin Dumba <cocodini@hotmail.com>
2016-03-25 14:58:52 -0700

    adds static folder with index.js file

--- MSHappiness/server.js
+++ MSHappiness/server.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var port = process.env.port || 1337;
 var path = require('path');
 var app = express();
-var dum = fs.readFileSync(__dirname + "/dummyData.json", 'utf8');
 var dummyData = require('./dummyData.js');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname+'/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 app.listen(port, function () {
     console.log('listening on port: ' + port);
