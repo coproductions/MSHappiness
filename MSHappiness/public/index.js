e5e9f0ce89960a28722c4c3f33ebe45c463dd467
Constantin Dumba <cocodini@hotmail.com>
2016-03-28 13:23:44 -0700

    ads reddit video

--- MSHappiness/public/index.js
+++ MSHappiness/public/index.js
@@ -10,14 +10,14 @@ $(".helloButton").click(function () {
         url: "http://localhost:1337/test1",
 
     }).done(function (data) {
-        console.log('data', data);
+       // console.log('data', data);
         $('.hello').html(data[0].building);
     })
 });
 
 
 //example of a post request after a button click
-$(".postTest").click(function (){
+$(".postTest").click(function () {
     var postObj = { "text": $(".testInput").val() };
     console.log('postobj' , postObj);
     $.ajax({
@@ -26,7 +26,37 @@ $(".postTest").click(function (){
         url: "http://localhost:1337/test2",
         data: postObj
     }).done(function (msg) {
-        console.log("data saved: ", msg);
+       // console.log("data saved: ", msg);
         $(".testInput").val("")
     });
-})
+});
+
+//click event example that triggers an ajax call to get a random reddit video
+$(".showVideo").click(function () {
+    console.log('clicked');
+    
+    //ajax example, very basic 
+    $.ajax({
+        method: "GET",
+        dataType : "json",
+
+        url: "https://www.reddit.com/r/funnyvideos.json",
+
+    }).done(function (data) {
+       
+        $('.video').html(getIframe(data));
+    })
+});
+
+var getIframe = function (data) {
+    var rnd = Math.floor(Math.random() * 10);
+    console.log(rnd);
+    var h = data.data.children[rnd].data.media.oembed.height;
+    var w = data.data.children[rnd].data.media.oembed.width;
+    var url = data.data.children[rnd].data.media.oembed.url;
+    if (url.indexOf('watch?v=') > 0) {
+        var splitArr = url.split('watch?v=');
+        url = splitArr[0] + 'embed/' + splitArr[1];
+    }
+    return '<iframe width="' + w + '" height="' + h + '"src="' + url + '"></iframe>';;
+};
