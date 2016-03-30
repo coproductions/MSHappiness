7acfb542029fbd5cbd3eaf0000d485942cff78e4
satyamlp <satyamlp@yahoo.com>
2016-03-30 14:32:12 -0700

    some sample code

--- MSHappiness/public/index.js
+++ MSHappiness/public/index.js
@@ -47,6 +47,30 @@ $(".showVideo").click(function () {
     })
 });
 
+//$("#inputForm").submit(function () {
+$(".postData").click(function() {
+    var data = {
+        userName: $('.username').val(),
+        bldgNum: $('.building').val(),
+        status: $('.status').val(),
+        comments: $('.comments').val()
+    };
+//    alert("called");
+    $(".hello").html(data.userName+data.bldgNum+data.status+data.comments);
+    //var userName = $(".username").val();
+    var postObj = data;
+    console.log('postobj' , postObj);
+    $.ajax({
+        method: "POST",
+        //dataType : "html",
+        url: "http://localhost:1337/test2",
+        data: postObj
+    }).done(function (msg) {
+        // console.log("data saved: ", msg);
+        $(".hello").html(data[0].building);
+    });
+});
+
 var getIframe = function (data) {
     var rnd = Math.floor(Math.random() * 25);
     var h = data.data.children[rnd].data.media.oembed.height;
@@ -58,3 +82,132 @@ var getIframe = function (data) {
     }
     return '<iframe width="' + w + '" height="' + h + '"src="' + url + '"></iframe>';;
 };
+
+
+//var getPercentageData = function () {
+function getPercentageData()
+ {    /*
+    if (status == "happy")
+        var urlStr = "http interface to query happy. expecting an array of ints as return value" ;
+    else if (status == "sad")
+        var urlStr = "";
+    else 
+        var urlStr = "";
+
+    // uncomment when the API is implemented in server
+    $.ajax({
+        method: "GET",
+        url: urlStr
+
+    }).done(function (data) {
+        //computeAverage(data);
+       });
+     */
+    alert("called");
+    var data = new [
+        {
+            userName: "user1", 
+            bldgNum: "25", 
+            status: "happy", 
+            comments: ""
+        },
+        {
+            userName: "user2", 
+            bldgNum: "25", 
+            status: "happy", 
+            comments: ""
+        },
+        {
+            userName: "user3", 
+            bldgNum: "25", 
+            status: "happy", 
+            comments: ""
+        },
+        {
+            userName: "user4", 
+            bldgNum: "25", 
+            status: "sad", 
+            comments: ""
+        },
+        {
+            userName: "user5", 
+            bldgNum: "25", 
+            status: "sad", 
+            comments: ""
+        },
+        {
+            userName: "user6", 
+            bldgNum: "25", 
+            status: "neutral", 
+            comments: ""
+        }];
+    
+    alert("called 2");
+    var happyCount, sadCount, neutralCount, otherCount = 0;
+    var len = data.Length;
+    for (var i = 0; i < len; i++) {
+        var status = data[i].status;
+        if (status == "happy") {
+            happyCount = happyCount + 1;
+        } else if (status == "sad") {
+            sadCount++;
+        } else if (status == "neutral") {
+            neutralCount++;
+        } else {
+            otherCount++;
+        }
+    }
+
+    if (len > 0) {
+        happyCount = (happyCount / len) * 100;
+        sadCount = (sadCount / len) * 100;
+        neutralCount = (neutralCount / len) * 100;
+        otherCount = (otherCount / len) * 100;
+    }
+    var percentageData = new {
+        happCount: happCount,
+        sadCount: sadCount,
+        neutralCount: neutralCount,
+        otherCount: otherCount
+    };
+    
+    alert(percentageData.happyCount);
+    var d = new [200, 60, 15, 10];
+    return d;
+
+    return [200, 60, 15, 10];
+    //alert(200);
+    //return percentageData;
+}
+
+var computePercentage = function (v) {
+    
+    if (len > 0) {
+        sum = sum / len;
+    }
+    return sum;
+}
+
+
+// <form id="inputForm">
+// <input type="submit" value="submit"><br><br />
+//</form>
+/*
+ *
+ <div>hello from index</div>
+    <button class="helloButton">click me</button>
+    <div class="hello"></div>
+    <input class="testInput" />
+    <button class="postTest">post this</button>
+    <button class="showVideo">show me a video</button>
+    <div class="video">
+        <!--<iframe class="videoFrame" width="420" height="315"
+        src="">-->
+   <!-- </iframe>-->
+
+    </div>
+    <video></video>
+
+    <button class="PieChart">PieChart</button>
+
+ * */
